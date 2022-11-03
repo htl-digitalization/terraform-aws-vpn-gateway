999baf5e76bceba2177b0f1549fb271358391828
HieuTrLuu <thl1g15@outlook.com>
2022-11-03 10:09:16 +0700

    Add vars to variables.tf

--- examples/complete-vpn-gateway/variables.tf
+++ examples/complete-vpn-gateway/variables.tf
@@ -1,5 +1,35 @@
 variable "vpc_private_subnets" {
   description = "List of private subnets"
   type        = list(string)
-  default     = ["10.10.11.0/24", "10.10.12.0/24", "10.10.13.0/24"]
+}
+
+variable "vpc_public_subnets" {
+  description = "List of public subnets"
+  type        = list(string)
+}
+
+variable "customer_ip" {
+  description = "Customer connected UO"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC id of eks cluster"
+  type        = string
+}
+
+
+variable "subnet_id" {
+  description = "1 Subnet id of eks cluster"
+  type        = string
+}
+
+variable "routetable_ids" {
+  description = "route table ids"
+  type        = list(string)
+}
+
+variable "ec2_sg_group" {
+  description = "ec2 sg group"
+  type        = list(string)
 }
